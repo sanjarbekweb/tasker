2611959e3780411e93e66adaa12b61d7b4ad4e5a
Sanjarbek <sanjarbeks650@gmail.com>
2026-08-27 02:18:34 +0500

    fix(gradle): use subprojects block to avoid project.afterEvaluate error on root project

--- plugins/with-android-min-sdk.js
+++ plugins/with-android-min-sdk.js
@@ -4,10 +4,10 @@ const withAndroidMinSdk = (config) => {
   return withProjectBuildGradle(config, (modConfig) => {
     if (modConfig.modResults.language === "groovy") {
       const subprojectFix = `
-allprojects {
-    afterEvaluate { project ->
-        if (project.plugins.hasPlugin("com.android.library") || project.plugins.hasPlugin("com.android.application")) {
-            project.android {
+subprojects { subproject ->
+    afterEvaluate {
+        if (subproject.plugins.hasPlugin("com.android.library")) {
+            subproject.android {
                 defaultConfig {
                     minSdkVersion = 24
                 }
@@ -16,7 +16,7 @@ allprojects {
     }
 }
 `;
-      if (!modConfig.modResults.contents.includes("defaultConfig {\n                    minSdkVersion = 24")) {
+      if (!modConfig.modResults.contents.includes('subproject.plugins.hasPlugin("com.android.library")')) {
         modConfig.modResults.contents += "\n" + subprojectFix;
       }
     }
